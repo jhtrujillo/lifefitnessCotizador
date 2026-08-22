f4180057c08241f2f1b6ad8d1974e2a1f4927d17
jhtrujillo <jhon.murdock@gmail.com>
2026-08-22 11:33:48 -0500

    Clean up temporary test scripts and logs

--- find_culprit2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$html = file_get_contents('stripped.html');
-$doc = new DOMDocument();
-@$doc->loadHTML('<?xml encoding="utf-8" ?>' . $html);
-$tables = $doc->getElementsByTagName('table');
-echo $doc->saveHTML($tables->item(0));
--- test_crash3.php
+++
@@ -1,4 +0,0 @@
-<?php
-$html = file_get_contents('new_crash.html');
-$html = preg_replace('/^<table class="pdf-frame">.*?<tbody><tr><td>(.*?)<\/td><\/tr><\/tbody><\/table>.*$/is', '$1', $html);
-file_put_contents('stripped.html', $html);
